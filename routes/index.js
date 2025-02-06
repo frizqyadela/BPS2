dd9584b3dbdfd4551f152dae3b375043786e7e43
LENOVO <frizqyadelaa@gmail.com>
2025-02-06 21:26:35 +0700

    yg pop up hapus belum

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('home', { layout: "layouthome" }); 
 });
 
 /* GET beranda page */
@@ -13,8 +12,32 @@ router.get('/beranda', function (req, res, next) {
 
 /* GET beranda page */
 router.get('/daftar', function (req, res, next) {
-  res.render('daftar', { title: 'Express', layout: "layout" });
+  res.render('daftar', { title: 'Daftar' });
 });
 
+/* GET beranda page */
+router.get('/tambahakun', function (req, res, next) {
+  res.render('tambahakun', { title: 'Tambah Akun' });
+});
+
+/* GET beranda page */
+router.get('/detail', function (req, res, next) {
+  res.render('detail', { title: 'Detail' });
+});
+
+/* GET beranda page */
+router.get('/dokumen', function (req, res, next) {
+  res.render('dokumen', { title: 'Dokumen' });
+});
+
+/* GET beranda page */
+router.get('/tambahkwitansi', function (req, res, next) {
+  res.render('tambahkwitansi', { title: 'Tambah Kwitansi' });
+});
+
+/* GET beranda page */
+router.get('/editkwitansi', function (req, res, next) {
+  res.render('editkwitansi', { title: 'Edit Kwitansi' });
+});
 
 module.exports = router;
